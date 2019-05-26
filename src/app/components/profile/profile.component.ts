82322bf15b52a55edeb14d077167b7df8f6f841c
pshahu24 <pujashahu92@gmail.com>
2019-05-26 15:38:21 +0530

    css changes

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ProfileComponent implements OnInit {
   dateValue:any;
   selectedValues: string[] = ['basic','captialgain','foreignincome'];
+  selectedbuisnessValues:string[] = ['smallBusiness','mediumBusinsess','largeBusinsess'];
   constructor() { }
 
   ngOnInit() {
