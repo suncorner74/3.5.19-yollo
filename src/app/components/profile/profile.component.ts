dd1770dac2a5cb9d564376189a6f1e17067e6108
Puja shahu <pujashahu92@gmail.com>
2019-05-26 16:25:59 +0530

    validation

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -6,12 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-  dateValue:any;
-  selectedValues: string[] = ['basic','captialgain','foreignincome'];
-  selectedbuisnessValues:string[] = ['smallBusiness','mediumBusinsess','largeBusinsess'];
-  constructor() { }
+  dateValue: any;
+  selectedValues: string[] = ['basic', 'captialgain', 'foreignincome'];
+  profileForm: FormGroup;
+  salaryType = false;
+  buisnessCheck = false;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.profileForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', [Validators.required, Validators.minLength(10)]],
+      pancard: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      userType: ['', Validators.required],
+      salryType:[''],
+      businesstype:[''],
+      gst:[''],
+      reg:['']
+    });
+  }
+
+  salryType(event) {
+    this.salaryType = true;
+    this.buisnessCheck = false;
+  }
+
+  buisnessType($event) {
+    this.salaryType = false;
+    this.buisnessCheck = true;
+  }
+  onSubmit(){
+
   }
 
 }
